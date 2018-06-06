67f44e3fb544f06616bd6439fe0b4b7caa4dde52
Emma Lejeck <nuck@kitsu.io>
2018-06-06 14:39:54 -0700

    Add User#max_pro_streak

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180606201703) do
+ActiveRecord::Schema.define(version: 20180606202637) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -1559,6 +1559,7 @@ ActiveRecord::Schema.define(version: 20180606201703) do
     t.integer  "ao_pro"
     t.string   "ao_imported"
     t.datetime "pro_started_at"
+    t.integer  "max_pro_streak"
   end
 
   add_index "users", ["ao_id"], name: "index_users_on_ao_id", unique: true, using: :btree
